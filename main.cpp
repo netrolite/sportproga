03aa12982484628cc26e7106eabfb54c13a198b0
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2026-01-09 23:12:42 +0300

    I

--- main.cpp
+++ main.cpp
@@ -1,75 +1,58 @@
-#include <cstring>
 #include <fstream>
+#include <iostream>
 
-static const char *const morseCodes[55] = {
-    ".-",     "-...",   "-.-.",   "-..",     ".",      "..-.",     "--.",
-    "....",   "..",     ".---",   "-.-",     ".-..",   "--",       "-.",
-    "---",    ".--.",   "--.-",   ".-.",     "...",    "-",        "..-",
-    "...-",   ".--",    "-..-",   "-.--",    "--..",   "-----",    ".----",
-    "..---",  "...--",  "....-",  ".....",   "-....",  "--...",    "---..",
-    "----.",  ".-.-.-", "--..--", "..--..",  ".----.", "-.-.--",   "-..-.",
-    "-.--.",  "-.--.-", ".-...",  "---...",  "-.-.-.", "-...-",    ".-.-.",
-    "-....-", "..--.-", ".-..-.", "...-..-", ".--.-.", "...---..."};
-
-static const char *const symbols[55] = {
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",  "K", "L", "M",  "N",
-    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",  "Y", "Z", "0",  "1",
-    "2", "3", "4", "5", "6", "7", "8", "9", ".", ",",  "?", "'", "!",  "/",
-    "(", ")", "&", ":", ";", "=", "+", "-", "_", "\"", "$", "@", "SOS"};
-
-const char *morseToChar(const char *code) {
-  // if (code[0] == '\0')
-  //   return nullptr;
-
-  for (int i = 0; i < 55; ++i) {
-    if (strcmp(morseCodes[i], code) == 0)
-      return symbols[i];
-  }
-  return nullptr;
+void destroy(long long *arr) {
+  if (arr != nullptr)
+    delete[] arr;
 }
 
 int main() {
   std::ifstream in("input.txt");
   std::ofstream out("output.txt");
 
-  char ch;
-  char buffer[20];
-  int idx = 0;
-  int spaceCount = 0;
+  int n;
+  in >> n;
 
-  while (in.get(ch)) {
-    if (ch == '.' || ch == '-') {
-      // new word
-      if (spaceCount >= 3) {
-        out << " ";
-      }
+  long long *poly1 = new long long[n];
+  for (int i = 0; i < n; ++i)
+    in >> poly1[i];
 
-      buffer[idx++] = ch;
-      buffer[idx] = '\0';
-      spaceCount = 0;
-    } else if (ch == ' ' || ch == '\n' || ch == '\r') {
-      if (idx > 0) {
-        const char *res = morseToChar(buffer);
-        if (res)
-          out << res;
+  int m;
+  in >> m;
 
-        idx = 0;
-        buffer[0] = '\0';
-      }
+  long long *poly2 = new long long[m];
+  for (int i = 0; i < m; ++i)
+    in >> poly2[i];
 
-      if (ch == ' ')
-        spaceCount++;
+  int reusltSize = n + m;
+  long long *result = new long long[reusltSize]();
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      result[i + j] += poly1[i] * poly2[j];
     }
   }
 
-  if (idx > 0) {
-    const char *res = morseToChar(buffer);
-    if (res)
-      out << res;
+  int printLen = 0;
+  for (int i = reusltSize - 1; i >= 0; --i) {
+    if (result[i] != 0) {
+      printLen = i + 1;
+      break;
+    }
+  }
+
+  for (int i = 0; i < printLen; ++i) {
+    out << result[i];
+    if (i < printLen - 1)
+      out << " ";
   }
 
   in.close();
   out.close();
+
+  delete[] poly1;
+  delete[] poly2;
+  delete[] result;
 
   return 0;
 }
